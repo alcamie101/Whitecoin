cc57e015892d05c79b639d6dec11cf7c53e0bd02
Chris Long <alcamie@gmail.com>
2014-04-28 21:49:04 +1000

    MinAge for PoS 12hrs MaxAge 56d

--- src/main.cpp
+++ src/main.cpp
@@ -41,8 +41,8 @@ static CBigNum bnProofOfStakeLimit(~uint256(0) >> 20);
 static CBigNum bnProofOfWorkLimitTestNet(~uint256(0) >> 20);
 static CBigNum bnProofOfStakeLimitTestNet(~uint256(0) >> 20);
 
-unsigned int nStakeMinAge = 60 * 60 * 24 * 2;	// minimum age for coin age: 2d
-unsigned int nStakeMaxAge = -1;	// stake age of full weight: -1
+unsigned int nStakeMinAge = 60 * 60 * 24 * .5;	// minimum age for coin age: 12h
+unsigned int nStakeMaxAge = 60 * 60 * 24 * 56;	// stake age of full weight: 56d
 unsigned int nStakeTargetSpacing = 90;			// 90 sec block spacing
 
 int64 nChainStartTime = 1397400948;
